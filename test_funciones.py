acab55396c8d63a103368650478a2d94ecd53bf5
Your Name <you@example.com>
2026-06-21 22:44:23 -0300

    readme

--- test_funciones.py
+++ test_funciones.py
@@ -4,70 +4,70 @@ import unittest
 from primos import es_primo_division_ensayo, explicar_no_primo_DS, es_primo_fermat, explicar_no_primo_TF,es_primo_miller_rabin,explicar_no_primo_MR
 
 # TESTEO DE PRIMALIDAD POR DIVISIÓN ENSAYO
-# class TestPrimalidad(unittest.TestCase):
+class TestPrimalidad(unittest.TestCase):
 
-#     def test_numeros_primos(self):
-#         self.assertTrue(es_primo_division_ensayo(5))
-#         self.assertTrue(es_primo_division_ensayo(13))
+    def test_numeros_primos(self):
+        self.assertTrue(es_primo_division_ensayo(5))
+        self.assertTrue(es_primo_division_ensayo(13))
 
-#     def test_numeros_compuestos(self):
-#         self.assertFalse(es_primo_division_ensayo(4))
-#         self.assertFalse(es_primo_division_ensayo(9))
+    def test_numeros_compuestos(self):
+        self.assertFalse(es_primo_division_ensayo(4))
+        self.assertFalse(es_primo_division_ensayo(9))
 
-#     def test_casos_por_borde(self):
-#         self.assertFalse(es_primo_division_ensayo(0))
-#         self.assertFalse(es_primo_division_ensayo(1))
-#         self.assertFalse(es_primo_division_ensayo(-7))
+    def test_casos_por_borde(self):
+        self.assertFalse(es_primo_division_ensayo(0))
+        self.assertFalse(es_primo_division_ensayo(1))
+        self.assertFalse(es_primo_division_ensayo(-7))
 
 # TESTEO DE NO PRIMALIDAD POR DIVISIÓN ENSAYO
-# class TestNoPrimalidad(unittest.TestCase):
+class TestNoPrimalidad(unittest.TestCase):
 
-#     def test_numeros_primos(self):
-#         # assertTrue verifica que el resultado sea True
-#         self.assertTrue(explicar_no_primo_DS(6))
-#         self.assertTrue(explicar_no_primo_DS(14))
+    def test_numeros_primos(self):
+        # assertTrue verifica que el resultado sea True
+        self.assertTrue(explicar_no_primo_DS(6))
+        self.assertTrue(explicar_no_primo_DS(14))
 
-#     def test_numeros_compuestos(self):
-#         self.assertIsInstance(explicar_no_primo_DS(4),str)
-#         self.assertIsInstance(explicar_no_primo_DS(9),str)
+    def test_numeros_compuestos(self):
+        self.assertIsInstance(explicar_no_primo_DS(4),str)
+        self.assertIsInstance(explicar_no_primo_DS(9),str)
 
-#     def test_casos_por_borde(self):
-#         self.assertIsInstance(explicar_no_primo_DS(0),str)
-#         self.assertIsInstance(explicar_no_primo_DS(1),str)
-#         self.assertIsInstance(explicar_no_primo_DS(-7),str)
+    def test_casos_por_borde(self):
+        self.assertIsInstance(explicar_no_primo_DS(0),str)
+        self.assertIsInstance(explicar_no_primo_DS(1),str)
+        self.assertIsInstance(explicar_no_primo_DS(-7),str)
 #************************************************************************************************
 # TESTEO DE PRIMALIDAD POR TEST DE FERMAT
-# class TestPrimalidad(unittest.TestCase):
-#     def test_numeros_primos(self):
-#         self.assertTrue(es_primo_fermat(5))
-#         self.assertTrue(es_primo_fermat(13))
+class TestPrimalidad(unittest.TestCase):
+    def test_numeros_primos(self):
+        self.assertTrue(es_primo_fermat(5))
+        self.assertTrue(es_primo_fermat(13))
 
-#     def test_numeros_compuestos(self):
-#         self.assertFalse(es_primo_fermat(4))
-#         self.assertFalse(es_primo_fermat(9))
+    def test_numeros_compuestos(self):
+        self.assertFalse(es_primo_fermat(4))
+        self.assertFalse(es_primo_fermat(9))
 
-#     def test_casos_por_borde(self):
-#         self.assertFalse(es_primo_fermat(0))
-#         self.assertFalse(es_primo_fermat(1))
-#         self.assertFalse(es_primo_fermat(-7))
+    def test_casos_por_borde(self):
+        self.assertFalse(es_primo_fermat(0))
+        self.assertFalse(es_primo_fermat(1))
+        self.assertFalse(es_primo_fermat(-7))
 
 # TESTEO DE NO PRIMALIDAD POR TEST DE FERMAT
-# class TestNoPrimalidad(unittest.TestCase):
+class TestNoPrimalidad(unittest.TestCase):
 
-#     def test_numeros_primos(self):
+    def test_numeros_primos(self):
         
-#         self.assertTrue(explicar_no_primo_TF(6))
-#         self.assertTrue(explicar_no_primo_TF(14))
+        self.assertTrue(explicar_no_primo_TF(6))
+        self.assertTrue(explicar_no_primo_TF(14))
 
-#     def test_numeros_compuestos(self):
+    def test_numeros_compuestos(self):
         
-#         self.assertIsInstance(explicar_no_primo_TF(4),str)
-#         self.assertIsInstance(explicar_no_primo_TF(9),str)
+        self.assertIsInstance(explicar_no_primo_TF(4),str)
+        self.assertIsInstance(explicar_no_primo_TF(9),str)
 
-#     def test_casos_por_borde(self):
-#         self.assertIsInstance(explicar_no_primo_TF(0),str)
-#         self.assertIsInstance(explicar_no_primo_TF(1),str)
-#         self.assertIsInstance(explicar_no_primo_TF(-7),str)
+    def test_casos_por_borde(self):
+        self.assertIsInstance(explicar_no_primo_TF(0),str)
+        self.assertIsInstance(explicar_no_primo_TF(1),str)
+        self.assertIsInstance(explicar_no_primo_TF(-7),str)
 
 #************************************************************************************************
 # TESTEO DE PRIMALIDAD POR TEST DE MILLER-RABIN
